6cd5d632be417a6d00e1c36b92d71f6896c65654
51-nevix <69389178+51-nevix@users.noreply.github.com>
2021-08-19 22:35:52 +0300

    Add files via upload

---
+++ charwarns/sh_plugin.lua
@@ -0,0 +1,48 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Character warns"
+PLUGIN.author = "Nevix"
+PLUGIN.description = "Система предупреждений выдаваемых администраторами."
+    
+
+
+ix.command.Add("CharWarn", {
+    description = "Выдать предупреждение персонажу.",
+    adminOnly = true,
+    arguments = {
+        ix.type.character, ix.type.text
+    },
+    OnRun = function(self, client, character, reason)
+        local oldvalue = character:GetData("warn", 0)
+        character:SetData("warn", oldvalue+1)
+        local newvalue = character:GetData("warn", 1)
+        local ply = character:GetPlayer()
+
+        if string.len(reason) <1 then
+            reason = "Не указана."
+        end
+        
+        if newvalue >=3 then
+            character:Ban(60)
+            character:SetData("warn", oldvalue)
+            client:Notify("Персонаж " .. client:GetName() .. " был заблокирован за превышение допустимого количества предупреждений")
+        end
+    
+        player:ChatNotify("Вам было выдано предупреждение от " .. client:GetName() .. "  Комментарий: " .. reason .. ". Будьте осторожны! Если вы получите слишком много предупреждений, ваш персонаж будет временно, либо перманентно заблокирован.")
+        --  MsgC( Color( 255, 0, 0 ), "Вам было выдано предупреждение от " .. client:GetName() .. "  Комментарий: " .. reason .. ". Будьте осторожны! Если вы получите слишком много предупреждений, ваш персонаж будет временно, либо перманентно заблокирован.")
+        player:ChatNotify("Всего предупреждений: " .. newValue)
+        player:Notify("Вы были заморожены на 10 секунд. Ознакомьтесь с предупреждением!")
+        client:Notify("Успешно!")
+
+        ply:GodEnable()
+        ply:Freeze(true)
+        IsValid(ply)
+        timer.Simple( 10, function()
+            if IsValid(ply) then
+                ply:GodDisable()
+                ply:Freeze(false)
+            end    
+        end)
+
+    end
+})
